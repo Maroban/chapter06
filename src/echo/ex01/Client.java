571b6864b01ee4ba919374513e8f5e9b3b24655e
hi <hi@DESKTOP-EGCI8BH>
2021-06-18 13:05:50 +0900

    06/18 강의

--- src/echo/ex01/Client.java
+++ src/echo/ex01/Client.java
@@ -1,8 +1,15 @@
 package echo.ex01;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
 
@@ -14,10 +21,79 @@ public class Client {
 		System.out.println("====================================");
 
 		System.out.println("[서버에 연결을 요청합니다.]");
-		socket.connect(new InetSocketAddress("110.12.83.8", 10001)); // ip주소와 포트번호를 받고 세팅
+		socket.connect(new InetSocketAddress("192.168.0.126", 10001)); // ip주소와 포트번호를 받고 세팅
 
 		System.out.println("[서버에 연결되었습니다.]");
 		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		// Scanner 만들기 (원리 이해)
+		InputStream in = System.in; // System 클래스 안의 in이라는 메소드 사용
+		InputStreamReader sisr = new InputStreamReader(in);
+		BufferedReader sbr = new BufferedReader(sisr);
+		
+		
+		// println() 만들기 (원리 이해)
+		OutputStream out = System.out;  // System 클래스 안의 out이라는 메소드 사용
+		OutputStreamWriter sosw = new OutputStreamWriter(out);
+		BufferedWriter sbw = new BufferedWriter(sosw);
+		
+				
+		// 메세지 보내기용 스트림
+		OutputStream os = socket.getOutputStream();
+		OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
+		BufferedWriter bw = new BufferedWriter(osw);
+
+		// 메세지 받기용 스트림
+		InputStream is = socket.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+		BufferedReader br = new BufferedReader(isr);
+
+		// 메세지 보내기
+		String str = "안녕";
+
+		// 보내기
+		bw.write(str);
+		bw.newLine();
+		bw.flush();
+
+		// 1:1 채팅 메신저처럼 메세지 보내기(반복문 사용)
+		while (true) {
+
+			// 키보드 입력
+			String msg = sc.nextLine();
+			// String msg = sbr.readLine(); => 직접 만든 Scanner 사용
+
+			// /q 가 입력되면 종료.
+			if ("/q".equals(msg)) {
+				System.out.println("[접속이 종료되었습니다.]");
+				break;
+			}
+
+			// 보내기
+			bw.write(msg);
+			bw.newLine();
+			bw.flush();
+
+			// 메세지 출력
+			String reMsg = br.readLine();
+			System.out.println("[Server: " + reMsg + "]");
+		}
+
+		System.out.println("====================================");
+		System.out.println("<클라이언트 종료>");
+		/*
+		sbw.write("<클라이언트 종료>"); => 직접 만든 println() 사용
+		sbw.newLine();
+		*/
+		
+		sbw.close();
+		sbr.close();
+		sc.close();
+		br.close();
+		bw.close();
 		socket.close();
 	}
 
--- src/echo/ex01/Server.java
+++ src/echo/ex01/Server.java
@@ -1,6 +1,12 @@
 package echo.ex01;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -8,36 +14,64 @@ import java.net.Socket;
 public class Server {
 
 	public static void main(String[] args) throws IOException {
-		
-		ServerSocket serverSocket = new ServerSocket();  // 1번 ServerSocket 생성
-		
+
+		ServerSocket serverSocket = new ServerSocket(); // 1번 ServerSocket 생성
+
 		// bind(new InetSocketAddress)를 통해 ip주소와 포트 번호를 넣어야 한다.
-		serverSocket.bind(new InetSocketAddress("110.12.83.8", 10001));  // 2번 연결 요청을 기다리고 있다.
-		
+		serverSocket.bind(new InetSocketAddress("192.168.0.126", 10001)); // 2번 연결 요청을 기다리고 있다.
+
 		System.out.println("<서버 시작>");
 		System.out.println("====================================");
 		System.out.println("[연결을 기다리고 있습니다.]");
-		
-		Socket socket = serverSocket.accept();  // 5번 연결 요청이 오면 작동한다.
-		// 6번 accept()을 이용해서 Socket socket을 만든다.
-		
+
+		// 5번(accept) 2번에서 연결 요청 확인이 되면 연결 요청이 오면 작동한다.
+		Socket socket = serverSocket.accept();
+		// 6번(Socket) accept()을 이용해서 Socket socket을 만든다.
+
 		System.out.println("[클라이언트가 연결되었습니다.]");
-		
-		
+
+		// 메세지 받기용 스트림
+		InputStream is = socket.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+		BufferedReader br = new BufferedReader(isr);
+
+		// 메세지 보내기용 스트림
+		OutputStream os = socket.getOutputStream();
+		OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
+		BufferedWriter bw = new BufferedWriter(osw);
+
+		// 메세지 받기
+		String text = br.readLine();
+		System.out.println("받은 메세지: " + text);
+
+		// 1:1 채팅 메신저처럼 메세지 받기(반복문 사용)
+		while (true) {
+			// 키보드 입력 값 받기
+			String msg = br.readLine();
+
+			// 클라이언트에서 /q가 입력되면 null 값이 오며 종료
+			if (msg == null) {
+				System.out.println("[클라이언트 접속 종료]");
+				break;
+			}
+
+			// 입력 받은 값 출력
+			System.out.println("받은 메세지: " + msg);
+
+			// 보내기
+			bw.write(msg);
+			bw.newLine();
+			bw.flush();
+		}
+
+		System.out.println("====================================");
+		System.out.println("<서버 종료>");
+
+		bw.close();
+		br.close();
 		socket.close();
-		
 		serverSocket.close();
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
 	}
 
 }
